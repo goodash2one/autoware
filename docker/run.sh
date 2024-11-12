e3b02c50c890b4d8f83a3e9d42845de91868b6e0
goodash2one <105575464+goodash2one@users.noreply.github.com>
2024-11-12 17:25:31 +0900

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -10,16 +10,17 @@ IMAGE=$1
 
 XSOCK=/tmp/.X11-unix
 XAUTH=$XAUTHORITY 
-VULKAN=/usr/share/vulkan
+SHARED_DOCK_DIR=/shared_dir
+SHARED_HOST_DIR=/home/$USER/shared_dir
 
 DEVICES="--device /dev/snd --device /dev/dri"
 
-VOLUMES="--volume=$XSOCK:$XSOCK:ro	 
-	 --volume=$XAUTH:$XAUTH:ro
+VOLUMES="--volume=$XSOCK:$XSOCK:ro
+	 --volume=$XAUTH:/root/.Xauthority:ro
+	 --volume=$SHARED_HOST_DIR:$SHARED_DOCK_DIR:rw
 	 --volume=/media:/media:rw
 	 --volume=/dev/shm:/dev/shm:rw
 	 --volume=/dev/ttyUSB0:/dev/ttyUSB0:ro"
-	 #--volume=$VULKAN:$VULKAN:rw
 	 
 ENVIRONS="--env DISPLAY=$DISPLAY
 	  --env SDL_VIDEODRIVER=x11
@@ -31,12 +32,16 @@ GPU='--gpus all,"capabilities=graphics,utility,display,video,compute"'
 FULL_CMD="docker run -it \
 	--privileged \
 	--net=host \
+ 	--runtime=nvidia \
 	$GPU \
 	$DEVICES \
 	$ENVIRONS \
 	$VOLUMES \
-	$IMAGE"	 
+	$IMAGE"
 
+if [ ! -d $SHARED_HOST_DIR ]; then
+    mkdir $SHARED_HOST_DIR
+fi
 
 echo $FULL_CMD
 
