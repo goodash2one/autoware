02508e030162a1646aeabec521cef2ff7a9aa25e
Hojae Lee <goodash2one@gmail.com>
2024-11-13 23:06:40 +0900

    update for environment variable

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-then
-        echo "usage: $0 <repo:tag>"
-        exit
+if [ $# -ne 1 ] && [ $# -ne 2 ] && [ $# -ne 3 ]; then
+    echo "usage: $0 <repo:tag> [\"network\"] [id(0~101)]"
+    exit
 fi
 
 IMAGE=$1
@@ -24,7 +23,25 @@ VOLUMES="--volume=$XSOCK:$XSOCK:ro
 	 
 ENVIRONS="--env DISPLAY=$DISPLAY
 	  --env SDL_VIDEODRIVER=x11
-	  --env XAUTHORITY=$XAUTHORITY"
+	  --env XAUTHORITY=/root/.Xauthority"
+
+if [ "$2" == "network" ] || [ "$2" == "NETWORK" ] || \
+   [ "$2" == "net" ] || [ "$2" == "NET" ]; then
+    ENVIRONS="$ENVIRONS --env ROS_LOCALHOST_ONLY=0"
+elif [ "$2" == "local" ] || [ "$2" == "LOCAL" ] || \
+     [ -z "$2" ]; then
+    ENVIRONS="$ENVIRONS --env ROS_LOCALHOST_ONLY=1"
+else
+    echo "invalid arg: $2"
+    exit
+fi
+
+if [[ "$3" =~ ^[0-9]+$ ]] && [ "$3" -ge 0 ] && [ "$3" -le 101 ]; then
+    ENVIRONS="$ENVIRONS --env ROS_DOMAIN_ID=$3"
+elif [ -n "$3" ]; then
+    echo "invalid arg: $3"
+    exit
+fi
 
 GPU='--gpus all,"capabilities=graphics,utility,display,video,compute"' 
 #GPU='--gpus all'
